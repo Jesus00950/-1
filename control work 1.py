768ebdaa45902802fffc166a0b3d3e2a27aad52a
Jesus00950 <konstantinkazantsev2011@gmail.com>
2024-12-04 16:37:05 +0700

    Add files via upload

---
+++ control work 1.py
@@ -0,0 +1,44 @@
+#print('Казанцев Константин мф 72,средний вариант')
+
+#N1
+#
+# high= int(input('введите длину'))
+# width = int(input('введите ширину'))
+# length = int(input('введите высоту'))
+# print('ещё раз')
+# high2 = int(input('введите длину'))
+# width2 = int(input('введите ширину'))
+# length2= int(input('введите высоту'))
+# v = high*width*length
+# v2 = high2*width2*length2
+# if v < v2:
+#     print('второй объём больше')
+# elif v > v2:
+#     print('второй объём меньше')
+# elif v == v2:
+#     print('второй объём равен первому')
+#
+#
+# #N2
+#
+# h = 7
+# v = h + 2
+# m = v //4
+# for i in range(1, h+1):
+#     if (i <= m):
+#         print(" " * (m-i) + "*" * (2*i) + " " * (v-2*i-2*m) + "*" *(2*i) + " " * (m-i))
+#     else:
+#       print(" " * (i - 2*m+1) + "*" * (v-2*(i-2*m+1)) + " " * (i - 2*m+1))
+#
+# #N3
+#
+# def comparion(k, n):
+#     if k < n:
+#         print('всё хорошо')
+#     elif k == n:
+#             print('всё хорошо')
+#     else:
+#         print('ошибка')
+#
+#k = int(input('введите число'))
+#n = int(input('введите число'))
